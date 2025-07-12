c71e32a853eaf7b90d17c6235ac91a3682368449
Drew Tom <tom.drew261@gmail.com>
2025-07-11 23:13:08 -0500

    typo on brothers page

--- src/app/brothers/page.tsx
+++ src/app/brothers/page.tsx
@@ -44,7 +44,7 @@ export default function BrothersPage() {
         <section className="min-h-[120px] bg-[#141416] text-white py-10">
             <div className="max-w-[1100px] mx-auto px-4 w-full">
                 <h1 className="text-3xl md:text-5xl font-semibold flex justify-center items-center text-center mb-15">
-                Chapter Officers
+                Chapter Brothers
                 </h1>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full">
                     {roster.actives.map((officer, index) => (
